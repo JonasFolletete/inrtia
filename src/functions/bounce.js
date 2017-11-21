1c9e39aa5c2509afc55bc470dce7ead329d4f613
JonasFolletete <jonas.folletete@gmail.com>
2017-11-21 14:46:51 +0100

    Simplify bounce function

--- src/functions/bounce.js
+++ src/functions/bounce.js
@@ -1,16 +1,7 @@
-const frictionMultiplier = 100; // uniform friction through interpolation
+import elastic from './elastic';
 
 export default function (current, target, params, dTime, reset) {
-  if (reset) this.speed = 0;
-
-
-  const delta = current - target;
-  const spring = -delta / (params.friction / frictionMultiplier);
-  const damper = -params.rigidity * this.speed;
-  const acc = (spring + damper) / params.mass;
-
-  this.speed += acc * dTime;
-  const result = current + (this.speed * dTime);
+  const result = elastic.call(this, current, target, params, dTime, reset);
 
   if ((result - target) >= 0) {
     this.speed = -this.speed;
--- src/functions/elastic.js
+++ src/functions/elastic.js
@@ -3,7 +3,6 @@ const frictionMultiplier = 100; // uniform friction through interpolation
 export default function (current, target, params, dTime, reset) {
   if (reset) this.speed = 0;
 
-
   const delta = current - target;
   const spring = -delta / (params.friction / frictionMultiplier);
   const damper = -params.rigidity * this.speed;
